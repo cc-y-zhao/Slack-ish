388728bbc6dec45825150637d7f2c9b16f73e3d5
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-04 23:11:21 -0700

    added to do in models/user.py

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    #I think we need to take out the cascade='all-delete' here
     messages = db.relationship(
         'Message', back_populates='user', cascade="all, delete")
 
