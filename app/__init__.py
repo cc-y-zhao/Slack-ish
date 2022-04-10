874dcfd518410e1dde00e95ddea3b711c75ac230
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-04-10 15:36:34 -0700

    added slackish.ico to init file

--- app/__init__.py
+++ app/__init__.py
@@ -75,5 +75,5 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+        return app.send_static_file('slackish.ico')
     return app.send_static_file('index.html')
